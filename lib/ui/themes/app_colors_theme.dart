70d6a45f5da0c7b335331aabf993c3136022f7fd
Claylton Santos <clayltonsp@hotmail.com>
2024-09-05 17:42:32 -0300

    Add themes

---
+++ lib/ui/themes/app_colors_theme.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+
+abstract class AppColorsTheme {
+  static const Color primaryColor = Color(0xFF00B4CC);
+  static const Color greyColor = Color(0xFF7C9094);
+  static const Color whiteColor = Color(0xFFFFFFFF);
+}
---
+++ lib/ui/themes/app_text_style_theme.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/widgets.dart';
+import 'app_colors_theme.dart';
+
+abstract class AppTextStyleTheme {
+
+  static const TextStyle navBarMenuTitle = TextStyle(
+    color: AppColorsTheme.primaryColor,
+    fontSize: 10,
+    fontFamily: 'DMSans-Regular',
+  );
+}
