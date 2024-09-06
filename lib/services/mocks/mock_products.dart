57e0d5d8ed513cdd34ac6aa54e64b1634a8061d1
Claylton Santos <clayltonsp@hotmail.com>
2024-09-06 06:02:53 -0300

    Update mock

--- lib/services/mocks/mock_products.dart
+++ lib/services/mocks/mock_products.dart
@@ -29,11 +29,11 @@ abstract class ProductsMock {
         {
           "imagePath":
               "https://raw.githubusercontent.com/claylton/corelab_app_challenge/main/assets/images/mask.png",
-          "title": "Condicionador Ácido Fosfórico 37% - AllPrime",
-          "price": 290.90,
-          "oldPrice": 300.00,
-          "discount": 20,
-          "parcelsInfo": "Em até 12x de R\$ 249,00",
+          "title": "Máscaras Descartáveis Brancas - NTFlex",
+          "price": 8.78,
+          "oldPrice": 19.99,
+          "discount": 56,
+          "parcelsInfo": "Em até 12x de R\$ 0,73",
           "productState": "NOVO",
           "createdAt": DateTime.now().subtract(const Duration(days: 1)),
         },
@@ -49,11 +49,29 @@ abstract class ProductsMock {
         {
           "imagePath":
               "https://raw.githubusercontent.com/claylton/corelab_app_challenge/main/assets/images/mask.png",
-          "title": "Condicionador Ácido Fosfórico 37% - AllPrime",
+          "title": "Máscaras Descartáveis Brancas - NTFlex",
           "price": 29.90,
           "parcelsInfo": "Em até 12x de R\$ 249,00",
           "productState": "NOVO",
           "createdAt": DateTime.now().subtract(const Duration(days: 1)),
+        },
+        {
+          "imagePath":
+              "https://raw.githubusercontent.com/claylton/corelab_app_challenge/main/assets/images/mask.png",
+          "title": "Máscaras Descartáveis Brancas - NTFlex",
+          "price": 29.90,
+          "parcelsInfo": "Em até 12x de R\$ 249,00",
+          "productState": "NOVO",
+          "createdAt": DateTime.now().subtract(const Duration(days: 5)),
+        },
+        {
+          "imagePath":
+              "https://raw.githubusercontent.com/claylton/corelab_app_challenge/main/assets/images/mask.png",
+          "title": "Máscaras Descartáveis Brancas - NTFlex",
+          "price": 29.90,
+          "parcelsInfo": "Em até 12x de R\$ 249,00",
+          "productState": "NOVO",
+          "createdAt": DateTime.now().subtract(const Duration(days: 6)),
         },
       ],
     };
