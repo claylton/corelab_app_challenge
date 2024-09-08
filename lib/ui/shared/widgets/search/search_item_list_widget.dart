35911680125fd718867b2b1215ab4f7543768061
Claylton Santos <clayltonsp@hotmail.com>
2024-09-08 07:42:25 -0300

    Search for de history

--- lib/ui/shared/widgets/search/search_item_list_widget.dart
+++ lib/ui/shared/widgets/search/search_item_list_widget.dart
@@ -1,6 +1,9 @@
+import 'package:corelab_app_challenge/blocs/search_bloc.dart';
+import 'package:corelab_app_challenge/ui/pages/search_page.dart';
 import 'package:corelab_app_challenge/ui/shared/widgets/search/search_item_widget.dart';
 import 'package:corelab_app_challenge/ui/themes/app_text_style_theme.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 class SearchItemListWidget extends StatelessWidget {
   final List<String> items;
@@ -9,6 +12,8 @@ class SearchItemListWidget extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    SearchBloc bloc = Provider.of<SearchBloc>(context);
+
     return Column(
       crossAxisAlignment: CrossAxisAlignment.start,
       children: [
@@ -27,7 +32,20 @@ class SearchItemListWidget extends StatelessWidget {
               children: [
                 SearchItemWidget(
                   name: items[index],
-                  onTap: () {},
+                  onTap: () {
+                    bloc.search(items[index]);
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                        builder: (context) => SearchPage(
+                          textEditingController: TextEditingController(text: items[index]),
+                          autoFocus: false,
+                          showFilter: true,
+                          showHistory: false,
+                        ),
+                      ),
+                    );
+                  },
                 )
               ],
             );
